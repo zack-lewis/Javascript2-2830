a58786da0bf03aca8886ea82eda01480e3d97b80
Zack Lewis <zack@lima3.dev>
2023-02-18 21:45:01 -0600

    Fixing formatting

--- functions.js
+++
@@ -1,8 +0,0 @@
-function adjustContentMin() {
-    let menuSize = document.getElementById("body-nav"); 
-    let contentSize = document.getElementById("body-content");
-
-    if(contentSize < menuSize) {
-        contentSize = menuSize; 
-    }
-}
