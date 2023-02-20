31beba49948148886e1e5e4bd8b2b6a83f18b305
Zack Lewis <zack@lima3.dev>
2023-02-19 23:28:19 -0600

    page 2

--- pg2functions.js
+++ pg2functions.js
@@ -24,11 +24,24 @@ function fillPrevBtns() {
 
 function initLoad() {
     fillPrevBtns();
+    loadVideo(0);
+    delay(1000).then(fillBufferBar());
 }
 
 function loadVideo(index) {
     var videoLink = pg2videos[index].vPath;
-    document.getElementById("body-video").getElementsByTagName("source")[0].src = videoLink;
+    var vidPlayer = document.getElementById("body-video");
+    var vidSrc = document.createElement("source");
+
+    vidSrc.setAttribute("src",videoLink);
+    vidSrc.setAttribute("type","video/mp4");
+
+    while (vidPlayer.firstChild) {
+        vidPlayer.removeChild(vidPlayer.firstChild);
+      }
+
+    vidPlayer.appendChild(vidSrc);
+    vidPlayer.load();    
 }
 
 function changeVideoSize(newSize) {
@@ -37,17 +50,43 @@ function changeVideoSize(newSize) {
     let defaultH = 300;
     switch (newSize) {
         case 's':
-            vidPlayer.style.width = defaultW * 0.6 + "px";
-            vidPlayer.style.height = defaultH * 0.6 + "px";
+            vidPlayer.style.width = defaultW * 0.5 + "px";
+            vidPlayer.style.height = defaultH * 0.5 + "px";
             break;
         case 'm':
             vidPlayer.style.width = defaultW + "px";
             vidPlayer.style.height = defaultH + "px";
             break;
         case 'l':
-            vidPlayer.style.width = defaultW *  1.2 + "px";
-            vidPlayer.style.height = defaultH * 1.2 + "px";
+            vidPlayer.style.width = defaultW *  1.5 + "px";
+            vidPlayer.style.height = defaultH * 1.5 + "px";
         default:
             break;
     }
 }
+
+function togglePlay() {
+    var ppBtn = document.getElementById("playpause");
+    var vidPlayer = document.getElementById("body-video");
+
+    if(ppBtn.innerHTML == "Play") {
+        ppBtn.innerHTML = "Pause";
+        vidPlayer.play();
+    }
+    else {
+        ppBtn.innerHTML = "Play";
+        vidPlayer.pause();
+    }
+}
+
+function fillBufferBar() {
+    // This only works part of the time and I have no idea why, unless its reloading the buffer and does not have a buffer index yet. which is pointless to have on there. 
+
+    var vidPlayer = document.getElementById("body-video");
+    var bufferBarElement = document.getElementById("bufferBar");
+    bufferBarElement.innerHTML = "Buffer Start: " + vidPlayer.buffered.start(0) + " End: " + vidPlayer.buffered.end(0);
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
